79c96b59ad29ce42371ed5bef919d10d84faba41
Mong'are Shem <mongareshem1@gmail.com>
2026-07-13 02:55:19 +0300

    Define custom colors

--- generating_data/scatter_squares.py
+++ generating_data/scatter_squares.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, edgecolor='none', s=10) #edgecolors=[]/'none'
+plt.scatter(x_values, y_values, color='red', edgecolors='none', s=10)
+# plt.scatter(x_values, y_values, color=(0, 0.8, 0), edgecolors='none', s=10)
 
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
