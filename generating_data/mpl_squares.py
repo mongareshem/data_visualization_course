0d7465b56792e6772463b5a0b8379519ecb63cb9
Mong'are Shem <mongareshem1@gmail.com>
2026-07-13 01:51:35 +0300

    Correct the plot

--- generating_data/mpl_squares.py
+++ generating_data/mpl_squares.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
 plt.title("Square Numbers",
@@ -8,5 +9,6 @@ plt.title("Square Numbers",
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Values", fontsize=14)
 plt.tick_params(axis='both',labelsize=14)
-plt.plot(squares)
+
+plt.plot(input_values, squares, linewidth=3)
 plt.show()
