c0b1982c7c8b35c4e589a6eb9190a17f3276d0fc
Mong'are Shem <mongareshem1@gmail.com>
2026-07-17 23:03:45 +0300

    Add plot points

--- generating_data/rw_visual.py
+++ generating_data/rw_visual.py
@@ -5,12 +5,13 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk and plot the points
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
+    # Plot the points and show the plot
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
-                cmap=plt.cm.Blues, edgecolors='none', s=8)
+                cmap=plt.cm.Blues, edgecolors='none', s=1)
 
     # Emphasize the first and last points.
     plt.scatter(0, 0, c='green', edgecolors='none', s=50)
