f9bb1a71f9ba78b1981a27e357ea6689105e8a7f
Mong'are Shem <mongareshem1@gmail.com>
2026-07-18 01:46:20 +0300

    Roll dice of different sizes

---
+++ generating_data/different_dice.py
@@ -0,0 +1,47 @@
+import pygal
+
+from random import randint
+
+class Die:
+    """A class representing a single die."""
+
+    def __init__(self, num_sides=6):
+        """Assume a six-sided die"""
+        self.num_sides = num_sides
+
+    def roll(self):
+        """Return a random value between 1 and the number of sides"""
+        return randint(1, self.num_sides)
+
+die_1 = Die()
+die_2 = Die(10)
+
+# Make some rolls and store the results in a list
+results = []
+for roll_num in range(50000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+print(results)
+
+# Analyze the results
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
+# Visualize the results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling a D6 and a D10 50,000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9',
+                 '10', '11', '12', '13', '14', '15', '16']
+hist.x_title ='Result'
+hist.y_title = 'Frequency of the Result'
+
+hist.add('D6 + D10', frequencies)
+hist.render_to_file('different_dice_visual.svg')
