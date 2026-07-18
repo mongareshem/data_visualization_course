a30eb1d5d7ced4bff7d21518fc017bcd092bbfd1
Mong'are Shem <mongareshem1@gmail.com>
2026-07-19 02:14:15 +0300

    Modify values in the x and y lists

--- generating_data/random_walk.py
+++ generating_data/random_walk.py
@@ -18,12 +18,12 @@ class RandomWalk:
         while len(self.x_values) < self.num_points:
 
             # Decide which direction to go
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
+            x_direction = choice([1, -1]) # Try [-1,-1], [1,1]
+            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
             x_step = x_direction * x_distance
 
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
+            y_direction = choice([1, -1]) # Try [-1,-1], [1,1]
+            y_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
             y_step = y_direction * y_distance
 
             # Reject moves that go nowhere
