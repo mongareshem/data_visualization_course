77dc58036ad10f3ccad817c8688a6e420c6138e8
Mong'are Shem <mongareshem1@gmail.com>
2026-07-19 01:07:45 +0300

    Smooth the edges

--- generating_data/exercise_cubes_scatter.py
+++ generating_data/exercise_cubes_scatter.py
@@ -4,7 +4,8 @@ input_values = list(range(1, 5001))
 outputs = [x**3 for x in input_values]
 
 # plt.scatter(input_values, outputs, color='purple', edgecolors='none' ,s=3)
-plt.scatter(input_values, outputs, c=input_values, cmap=plt.cm.rainbow, s=3)
+plt.scatter(input_values, outputs, c=input_values, cmap=plt.cm.rainbow,
+            edgecolors='none', s=3, antialiased=True)
 
 plt.title('Cube Numbers',
           fontdict={'family':'Serif', 'fontsize':30, 'fontweight': 'normal', 'fontstyle': 'italic'})
@@ -15,4 +16,5 @@ plt.tick_params(axis='both', labelsize=14)
 plt.xlim(0, 6000)
 plt.ylim(0, 150000000000)
 
+plt.savefig('cubes_scatter.png', bbox_inches='tight')
 plt.show()
