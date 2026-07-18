9d6d05c2edc443ab5bb132a9f9f5627c30442607
Mong'are Shem <mongareshem1@gmail.com>
2026-07-19 00:46:06 +0300

    Use scatter to plot the cubes graph and colormap

---
+++ generating_data/exercise_cubes_scatter.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+input_values = list(range(1, 5001))
+outputs = [x**3 for x in input_values]
+
+# plt.scatter(input_values, outputs, color='purple', edgecolors='none' ,s=3)
+plt.scatter(input_values, outputs, c=input_values, cmap=plt.cm.rainbow, s=3)
+
+plt.title('Cube Numbers',
+          fontdict={'family':'Serif', 'fontsize':30, 'fontweight': 'normal', 'fontstyle': 'italic'})
+plt.xlabel('Values', fontsize=16)
+plt.ylabel('Cube of Values', fontsize=16)
+plt.tick_params(axis='both', labelsize=14)
+
+plt.xlim(0, 6000)
+plt.ylim(0, 150000000000)
+
+plt.show()
