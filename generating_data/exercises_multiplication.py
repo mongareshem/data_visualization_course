139d2212bd15568293a63ae204b7d9a595572e5f
Mong'are Shem <mongareshem1@gmail.com>
2026-07-19 04:29:31 +0300

    Create a visualization of multiplying the rolled dice results

--- generating_data/exercises_multiplication.py
+++ generating_data/exercises_multiplication.py
@@ -17,21 +17,21 @@ die_1 = Die()
 die_2 = Die()
 
 # Make some rolls and store the results in a list
-results = [die_1.roll() + die_2.roll() for roll_num in range(1000)]
+results = [die_1.roll() * die_2.roll() for roll_num in range(1000)]
 print(results)
 
 # Analyze the results
-max_result = die_1.num_sides + die_2.num_sides
-frequencies = [results.count(value) for value in range(2, max_result+1)]
+max_result = die_1.num_sides * die_2.num_sides
+frequencies = [results.count(value) for value in range(1, max_result+1)]
 print(frequencies)
 
 # Visualize the results
 hist = pygal.Bar()
 
-hist.title = "Results of rolling two D6 1000 times."
-hist.x_labels = [str(i) for i in range(2, 13)]
+hist.title = "Results of rolling and multiplying two D6 1000 times."
+hist.x_labels = [str(i) for i in range(1, max_result + 1)]
 hist.x_title ='Result'
 hist.y_title = 'Frequency of the Result'
 
 hist.add('D6 + D6', frequencies)
-hist.render_to_file('dice_visual.svg')
+hist.render_to_file('exercise_multiplication.svg')
