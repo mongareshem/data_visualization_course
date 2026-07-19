4adc35bf2695a9958f67481e2599340cd7ba5164
Mong'are Shem <mongareshem1@gmail.com>
2026-07-19 04:06:41 +0300

    Calculate and print the results and frequencies

--- generating_data/exerecises_three_dice.py
+++ generating_data/exerecises_three_dice.py
@@ -10,3 +10,15 @@ class Die:
     def roll(self):
         """Simulate a roll"""
         return randint(1, self.sides)
+
+
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
+
+results = [die_1.roll() + die_2.roll() + die_3.roll() for i in range(1000000)]
+print(results)
+
+max_result = die_1.sides + die_2.sides + die_3.sides
+frequencies = [results.count(value) for value in range(3, max_result + 1)]
+print(frequencies)
