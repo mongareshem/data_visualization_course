4188eb48dedd2f256601e2e5ea8cb1445962573f
Mong'are Shem <mongareshem1@gmail.com>
2026-07-19 04:16:19 +0300

    Add a histogram to display the results

--- generating_data/exerecises_three_dice.py
+++ generating_data/exerecises_three_dice.py
@@ -22,3 +22,13 @@ print(results)
 max_result = die_1.sides + die_2.sides + die_3.sides
 frequencies = [results.count(value) for value in range(3, max_result + 1)]
 print(frequencies)
+
+hist = pygal.Bar()
+
+hist.title = 'Results of rolling 3 D6 dice'
+hist.x_labels = [str(i) for i in range(3, max_result + 1)]
+hist.x_title = 'Results'
+hist.y_title = 'Frequencies of the results'
+
+hist.add('D6 + D6 + D6', frequencies)
+hist.render_to_file('exercises_three_dice.svg')
