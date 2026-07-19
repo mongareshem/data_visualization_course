1b04ab5cf52e6154e938864a76ce86585997041f
Mong'are Shem <mongareshem1@gmail.com>
2026-07-19 03:47:43 +0300

    Add a histogram to display the results

--- generating_data/exercises_two_d8s.py
+++ generating_data/exercises_two_d8s.py
@@ -15,9 +15,19 @@ class Die:
 die_1 = Die()
 die_2 = Die()
 
-results = [die_1.roll() + die_2.roll() for i in range(20000000)]
+results = [die_1.roll() + die_2.roll() for i in range(1000000)]
 print(results)
 
 max_result = die_1.sides + die_2.sides
 frequencies = [results.count(value) for value in range(2, max_result + 1)]
 print(frequencies)
+
+hist = pygal.Bar()
+
+hist.title = 'Results of rolling two D8 dice'
+hist.x_labels = [str(i) for i in range(2, max_result + 1)]
+hist.x_title = 'Results'
+hist.y_title = 'Frequency of the results'
+
+hist.add('D8 + D8', frequencies)
+hist.render_to_file('exercise_dice.svg')
